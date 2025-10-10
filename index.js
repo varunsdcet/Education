b44d3af76876923996c7c196595e8f70cc75f807
BABASHYAM <babashyam@GANESHs-MacBook-Pro.local>
2025-10-10 13:50:47 +0530

    dd

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 const JWT_SECRET = "your_secret_key";
 
 // --- MongoDB Connection ---
-const MONGO_URI = "mongodb+srv://varunsinghal78_db_user:xRbG512ylHcUMpfL@cluster0.mjjsjk9.mongodb.net/school_db?retryWrites=true&w=majority";
+const MONGO_URI = "mongodb+srv://varunsinghal78_db_user:xRbG512ylHcUMpfL@cluster0.mjjsjk9.mongodb.net/school?retryWrites=true&w=majority";
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
